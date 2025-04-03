fe0cd5463902ee8ed18cf6db01efa3d9372647f8
Milan Tosic <mtosic0450@gmail.com>
2025-04-03 11:41:03 +0200

    Fix build errors for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://UnlimitedEdition.github.io', // Osnovni domen za GitHub Pages
-  // base: '/LogicLabs-Network', // Privremeno uklonjeno radi testa build-a
+  base: '/LogicLabs-Network', // Vraćena base opcija
   server: {
     host: true, // Omogućava pristup preko mrežne IP adrese (npr. 192.168.0.40)
     port: 8080, // Postavlja podrazumevani port na 8080
